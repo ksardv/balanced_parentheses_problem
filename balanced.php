592af0f4497881df67ce2a63cd7b0106387859c7
Kolyo Sardamov <Kolyo.Sardamov@bulpros.com>
2020-11-04 02:15:21 +0200

    balanced.php done according to psr1 and 2

--- balanced.php
+++ balanced.php
@@ -10,51 +10,46 @@ $strings = [
     '({)}' //no
 ];
 
-  function isValid(string $str): bool
-  {
+function isValid(string $str): bool
+{
     $arr = [];
     $splitString = str_split($str);
-    foreach($splitString as $st) {
-      //  If the input string contains an opening parenthesis,
+    
+    foreach ($splitString as $st) {
+      //  If the input string contains an opening bracket,
       //  push in on to the array.
-      if ($st == '(' || $st == '{' || $st == '[') {
-        array_push($arr, $st);
-      } else {
-      // In the case of valid parentheses, the array cannot be 
-      // be empty if a closing parenthesis is encountered.
-        if(count($arr) == 0) {
-            return false;
-        }
+        if ($st == '(' || $st == '{' || $st == '[') {
+            array_push($arr, $st);
+        } else {
+      // In the case of valid bracket, the array cannot be
+      // be empty if a closing bracket is encountered.
+            if (count($arr) == 0) {
+                return false;
+            }
         // If the input string contains a closing bracket,
-        // then remove the corresponding opening parenthesis if
+        // then remove the corresponding opening bracket if
         // present.
-        $top = end($arr);
-        if($st == ')' && $top == '(' ||
-        $st == '}' && $top == '{' ||
-        $st == ']' && $top == '['){
-          array_pop($arr);
+            $top = end($arr);
+            if ($st == ')' && $top == '(' ||
+            $st == '}' && $top == '{' ||
+            $st == ']' && $top == '[') {
+                array_pop($arr);
+            }
         }
-      }
-      }
-    //  Checking the status of the stack to determine the
-    //  validity of the string.
-    if(count($arr) == 0) {
-      return true;
-    } else {
-      return false;
     }
-  }
-  
-$start_time = microtime(true);
-
-foreach ($strings as $string){
-    if(isValid($string)){
-        echo("Yes\n");
-    }else{
-        echo("No\n");
-    }   
+    //  Checking the status of the array to determine the
+    //  validity of the string.
+    if (count($arr) == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-$end_time = microtime(true);
-$execution_time = ($end_time - $start_time);
-echo " It takes ". $execution_time." seconds to execute the script";
+foreach ($strings as $str) {
+    if (isValid($str)) {
+        echo("Yes\n");
+    } else {
+        echo("No\n");
+    }
+}
